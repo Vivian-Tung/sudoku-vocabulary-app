3168da04a2420f0fa7229ab62b975db2e455747c
Simon Yu <simonyu180@gmail.com>
2023-02-06 10:59:15 -0800

    Created new view class and attributes files

---
+++ app/src/main/java/com/example/sudokuvocabulary/SudokuGrid.java
@@ -0,0 +1,14 @@
+package com.example.sudokuvocabulary;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class SudokuGrid extends View {
+
+    public SudokuGrid(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}

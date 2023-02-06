902f557b595ea26684769e8172c011f54dcebfc6
Simon Yu <simonyu180@gmail.com>
2023-02-06 11:06:58 -0800

    Defined boardColor attribute for Sudoku Grid

--- app/src/main/java/com/example/sudokuvocabulary/SudokuGrid.java
+++ app/src/main/java/com/example/sudokuvocabulary/SudokuGrid.java
@@ -1,14 +1,31 @@
 package com.example.sudokuvocabulary;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class SudokuGrid extends View {
-
+    private final int boardColor;
     public SudokuGrid(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
+        TypedArray attributes = context.getTheme().obtainStyledAttributes(attrs,
+                R.styleable.SudokuGrid, 0, 0);
+
+        try {
+            boardColor = attributes.getInteger(R.styleable.SudokuGrid_gridColor, 0);
+        } finally {
+            attributes.recycle();
+        }
+    }
+
+    @Override
+    public void onMeasure(int width, int height) {
+        super.onMeasure(width, height);
+
+        int dimension = Math.min(this.getWidth(), this.getHeight());
     }
 }

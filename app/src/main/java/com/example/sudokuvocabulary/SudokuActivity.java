80e842c780c57516043e7e9e7731506a48c4a7f6
Simon Yu <simonyu180@gmail.com>
2023-02-06 14:34:05 -0800

    Implemented listener to get pressed cell

--- app/src/main/java/com/example/sudokuvocabulary/SudokuActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/SudokuActivity.java
@@ -3,6 +3,9 @@ package com.example.sudokuvocabulary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class SudokuActivity extends AppCompatActivity {
     @Override
@@ -16,5 +19,16 @@ public class SudokuActivity extends AppCompatActivity {
 
         SudokuView sudokuView = findViewById(R.id.sudokuGridView);
         sudokuView.setData(bundle);
+        sudokuView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                String tag = "SudokuActivity";
+                int cellRow = (int) Math.ceil(motionEvent.getY()/sudokuView.getCellSize());
+                int cellColumn = (int) Math.ceil(motionEvent.getX()/sudokuView.getCellSize());
+                Log.d(tag, "Cell Row: " + cellRow);
+                Log.d(tag, "Cell Column: " + cellColumn);
+                return false;
+            }
+        });
     }
 }

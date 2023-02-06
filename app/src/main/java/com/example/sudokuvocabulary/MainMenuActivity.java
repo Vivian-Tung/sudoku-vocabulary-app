bce0951ebf6f7b827d08e09c2eba8df2a29cbcc4
Simon Yu <simonyu180@gmail.com>
2023-02-05 22:47:10 -0800

    Set play button to start game activity

--- app/src/main/java/com/example/sudokuvocabulary/MainMenuActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/MainMenuActivity.java
@@ -2,12 +2,26 @@ package com.example.sudokuvocabulary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
+    private Button mPlayButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        mPlayButton = (Button) findViewById(R.id.main_menu_play_button);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }

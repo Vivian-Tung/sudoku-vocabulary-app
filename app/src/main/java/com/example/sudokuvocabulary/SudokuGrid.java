7cf39e085fdbbd02262ba0aa9fccc1f17ec70172
Simon Yu <simonyu180@gmail.com>
2023-02-06 11:13:36 -0800

    Overrided onDraw method to paint grid border

--- app/src/main/java/com/example/sudokuvocabulary/SudokuGrid.java
+++ app/src/main/java/com/example/sudokuvocabulary/SudokuGrid.java
@@ -2,13 +2,16 @@ package com.example.sudokuvocabulary;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class SudokuGrid extends View {
-    private final int boardColor;
+    private final int gridColour;
+    private final Paint gridColourPaint = new Paint();
     public SudokuGrid(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
@@ -16,7 +19,7 @@ public class SudokuGrid extends View {
                 R.styleable.SudokuGrid, 0, 0);
 
         try {
-            boardColor = attributes.getInteger(R.styleable.SudokuGrid_gridColor, 0);
+            gridColour = attributes.getInteger(R.styleable.SudokuGrid_gridColor, 0);
         } finally {
             attributes.recycle();
         }
@@ -27,5 +30,17 @@ public class SudokuGrid extends View {
         super.onMeasure(width, height);
 
         int dimension = Math.min(this.getWidth(), this.getHeight());
+
+        setMeasuredDimension(dimension, dimension);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        gridColourPaint.setStyle(Paint.Style.STROKE);
+        gridColourPaint.setColor(gridColour);
+        gridColourPaint.setStrokeWidth(14);
+        gridColourPaint.setAntiAlias(true);
+
+        canvas.drawRect(0,0, getWidth(), getHeight(), gridColourPaint);
     }
 }

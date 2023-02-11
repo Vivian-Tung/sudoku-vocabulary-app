8ae1cb87bf32a08825ee59943205d2ae82c58856
Simon Yu <simonyu180@gmail.com>
2023-02-11 00:55:17 -0800

    Implemented listeners for buttons to navigate to either main menu or new game activity

--- app/src/main/java/com/example/sudokuvocabulary/GameCompleteActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/GameCompleteActivity.java
@@ -2,7 +2,10 @@ package com.example.sudokuvocabulary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameCompleteActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class GameCompleteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_complete);
+
+        Button homeButton = (Button) findViewById(R.id.home_button);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GameCompleteActivity.this, MainMenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button restartButton = (Button) findViewById(R.id.restart_button);
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GameCompleteActivity.this, SudokuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

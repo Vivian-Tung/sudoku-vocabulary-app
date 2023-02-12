0b198ce6fd445670112332d535856d5e37b81293
Adam <ama139@sfu.ca>
2023-02-12 15:24:47 -0800

    -got menu button to go to word bank NEED TO WORK: -changing color of button -button usage for creating new word list

--- app/src/main/java/com/example/sudokuvocabulary/MainMenuActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/MainMenuActivity.java
@@ -2,13 +2,28 @@ package com.example.sudokuvocabulary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        button1=findViewById(R.id.main_menu_word_bank_button);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainMenuActivity.this,MainMenuActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }

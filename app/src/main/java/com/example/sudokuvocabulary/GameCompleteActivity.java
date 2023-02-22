8e1a355d6ac0dde2520aa0c544f6cd6e7a838113
Simon7896 <55372007+Simon7896@users.noreply.github.com>
2023-02-22 03:06:14 -0800

    Implemented feature to replay game with previous word list

--- app/src/main/java/com/example/sudokuvocabulary/GameCompleteActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/GameCompleteActivity.java
@@ -1,35 +1,45 @@
 package com.example.sudokuvocabulary;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class GameCompleteActivity extends AppCompatActivity {
+
+    private String[] mWords, mTranslations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_complete);
 
+        mWords = getIntent().getStringArrayExtra(getString(R.string.words_key));
+        mTranslations = getIntent().getStringArrayExtra(getString(R.string.translations_key));
+
         Button homeButton = (Button) findViewById(R.id.home_button);
-        homeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(GameCompleteActivity.this, MainMenuActivity.class);
-                startActivity(intent);
-            }
+        homeButton.setOnClickListener(view -> {
+            Intent intent = new Intent(GameCompleteActivity.this, MainMenuActivity.class);
+            startActivity(intent);
         });
 
         Button restartButton = (Button) findViewById(R.id.restart_button);
-        restartButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(GameCompleteActivity.this, SudokuActivity.class);
-                startActivity(intent);
-            }
+        restartButton.setOnClickListener(view -> {
+            Intent intent = newIntent(
+                    GameCompleteActivity.this, mWords, mTranslations);
+            startActivity(intent);
         });
+
+
+    }
+    @NonNull
+    public Intent newIntent(Context packageContext, String[] words, String[] translations) {
+        Intent intent = new Intent(packageContext, SudokuActivity.class);
+        intent.putExtra(getString(R.string.words_key), words);
+        intent.putExtra(getString(R.string.translations_key), translations);
+        return intent;
     }
 }

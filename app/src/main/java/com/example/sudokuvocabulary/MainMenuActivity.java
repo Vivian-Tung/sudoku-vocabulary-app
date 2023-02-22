84e4ad18d4931b730bf066b4789bd3746533a564
Simon7896 <55372007+Simon7896@users.noreply.github.com>
2023-02-21 17:16:47 -0800

    Renamed MainMenuActivity2.java to WordListActivity to avoid confusion with MainMenuActivity

--- app/src/main/java/com/example/sudokuvocabulary/MainMenuActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/MainMenuActivity.java
@@ -6,12 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.view.View;
-import android.widget.Button;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 
@@ -45,7 +40,7 @@ public class MainMenuActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainMenuActivity.this,MainMenuActivity2.class);
+                Intent intent = new Intent(MainMenuActivity.this, WordListsActivity.class);
                 startActivity(intent);
             }
         });

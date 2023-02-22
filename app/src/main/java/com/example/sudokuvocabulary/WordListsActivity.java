486fab5a2505e10231aeeb0386d41e6151a4eff6
Simon7896 <55372007+Simon7896@users.noreply.github.com>
2023-02-21 17:15:58 -0800

    Renamed MainMenuActivity2.java to WordListActivity to avoid confusion with MainMenuActivity

--- app/src/main/java/com/example/sudokuvocabulary/WordListsActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/WordListsActivity.java
@@ -7,29 +7,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainMenuActivity2 extends AppCompatActivity {
+public class WordListsActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button new_word_list_button;
 
-    private String[] categories = {"Food", "Transportation", "Shops", "Animals"};
+    private String[] categories = {"Animals", "Fruits"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu2);
+        setContentView(R.layout.activity_word_lists);
 
         new_word_list_button = (Button) findViewById(R.id.create_new_word_list_button);
 
         new_word_list_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainMenuActivity2.this, wordBankActivity.class);
+                Intent intent = new Intent(WordListsActivity.this, wordBankActivity.class);
                 startActivity(intent);
             }
         });
 
         WordListView existingWordLists = findViewById(R.id.existing_word_lists);
         existingWordLists.setWordListText(categories);
+        for (Button button: existingWordLists.getListButtons()) {
+            button.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
 
     }
 }

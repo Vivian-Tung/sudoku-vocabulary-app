f97f062b7612ded1107aea4ff3777c9f850931b9
Simon7896 <55372007+Simon7896@users.noreply.github.com>
2023-02-23 19:06:22 -0800

    Passed new table name into intent to be accessed by AnimalCategoryActivity

--- app/src/main/java/com/example/sudokuvocabulary/WordCategoryActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/WordCategoryActivity.java
@@ -7,21 +7,26 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class wordCategoryActivity extends AppCompatActivity {
+public class WordCategoryActivity extends AppCompatActivity {
 
     Button animal_category_button;
+    String tableName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_word);
 
+        tableName = getIntent().getStringExtra(getString(R.string.new_table_name_key));
+
         animal_category_button = (Button) findViewById(R.id.animal_list_button);
 
         animal_category_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(wordCategoryActivity.this, animalCategoryView.class);
+                Intent intent = new Intent(WordCategoryActivity.this,
+                        AnimalCategoryActivity.class);
+                intent.putExtra(getString(R.string.new_table_name_key), tableName);
                 startActivity(intent);
             }
         });

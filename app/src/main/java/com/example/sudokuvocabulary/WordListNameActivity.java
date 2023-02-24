7791472556d27a24340637d46eee5144d0b6c9b7
Simon7896 <55372007+Simon7896@users.noreply.github.com>
2023-02-23 19:05:16 -0800

    Text input is now checked for issues before switching activities

--- app/src/main/java/com/example/sudokuvocabulary/WordListNameActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/WordListNameActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class WordListNameActivity extends AppCompatActivity {
 
@@ -15,14 +16,26 @@ public class WordListNameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_word_list_name);
 
         EditText text = findViewById(R.id.table_name_input);
-
         Button confirmButton = findViewById(R.id.confirm_name_button);
+
         confirmButton.setOnClickListener(view -> {
             String listName = text.getText().toString();
-            Intent intent = new Intent(
-                    WordListNameActivity.this, WordCategoryActivity.class);
-            intent.putExtra(getString(R.string.new_table_name_key), listName);
-            startActivity(intent);
+
+            if (listName.length() == 0) {
+                Toast.makeText(this,
+                        "List name cannot be empty",
+                        Toast.LENGTH_SHORT).show();
+
+            } else if (listName.contains(" ")) {
+                Toast.makeText(this,
+                        "List name cannot have spaces",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Intent intent = new Intent(
+                        WordListNameActivity.this, WordCategoryActivity.class);
+                intent.putExtra(getString(R.string.new_table_name_key), listName);
+                startActivity(intent);
+            }
         });
     }
 }

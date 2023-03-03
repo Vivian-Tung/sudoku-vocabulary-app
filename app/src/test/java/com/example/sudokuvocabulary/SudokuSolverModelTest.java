aa65d0951baf70bdc3576a340e23bdc2fe8fd72a
Simon Yu <simonyu180@gmail.com>
2023-03-03 10:49:42 -0800

    Generated new test class for solver model

---
+++ app/src/test/java/com/example/sudokuvocabulary/SudokuSolverModelTest.java
@@ -0,0 +1,20 @@
+package com.example.sudokuvocabulary;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SudokuSolverModelTest {
+
+    @Test
+    public void solutions() {
+    }
+
+    @Test
+    public void solve() {
+    }
+
+    @Test
+    public void solver() {
+    }
+}

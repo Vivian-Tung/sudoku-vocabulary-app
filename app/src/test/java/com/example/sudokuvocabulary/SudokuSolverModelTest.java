5b5b579ab00cd8f69e9ad5d6f5fd108c6e250e45
Simon Yu <simonyu180@gmail.com>
2023-03-03 11:11:12 -0800

    Added basic test for solutions() and solve()

--- app/src/test/java/com/example/sudokuvocabulary/SudokuSolverModelTest.java
+++ app/src/test/java/com/example/sudokuvocabulary/SudokuSolverModelTest.java
@@ -8,10 +8,16 @@ public class SudokuSolverModelTest {
 
     @Test
     public void solutions() {
+        SudokuModel model = new SudokuModel();
+        int sol = SudokuSolverModel.solutions(model);
+        assertEquals(1, sol);
     }
 
     @Test
     public void solve() {
+        SudokuModel model = new SudokuModel();
+        SudokuSolverModel.solve(model);
+        assertTrue(model.isGridFilled());
     }
 
     @Test

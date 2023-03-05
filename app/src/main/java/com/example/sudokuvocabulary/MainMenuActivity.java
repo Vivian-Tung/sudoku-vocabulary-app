dfcb05881b15365e7ccc9db344f8cb42efc2f694
Vivian-Tung <viviantung@live.ca>
2023-03-04 17:04:22 -0800

    add dark theme toggle

--- app/src/main/java/com/example/sudokuvocabulary/MainMenuActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/MainMenuActivity.java
@@ -3,14 +3,20 @@ package com.example.sudokuvocabulary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
+
+
 
 import java.util.ArrayList;
 
@@ -19,6 +25,8 @@ public class MainMenuActivity extends AppCompatActivity {
     DBAdapter db;
     Button button1;
     private Button mPlayButton;
+    SwitchCompat mDarkSwitch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +59,24 @@ public class MainMenuActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //check for dark or light mode
+        mDarkSwitch = findViewById(R.id.darkSwitch);
+
+        mDarkSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                savePreferences("darkSwitch_Val", mDarkSwitch.isChecked());
+                if (mDarkSwitch.isChecked()) {
+                    AppCompatDelegate.setDefaultNightMode((AppCompatDelegate.MODE_NIGHT_YES));
+                    savePreferences("darkSwitch_Val", true);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode((AppCompatDelegate.MODE_NIGHT_NO));
+                    savePreferences("darkSwitch_Val", false);
+                }
+            }
+        });
+        loadSavedPreferences();
+
         setupTutorialButton();
 
         button1=findViewById(R.id.main_menu_word_bank_button);
@@ -74,6 +100,26 @@ public class MainMenuActivity extends AppCompatActivity {
             Intent intent = new Intent(MainMenuActivity.this, TutorialActivity.class);
             startActivity(intent);
         });
+    }
+
+    //save switch state
+    private void loadSavedPreferences(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mDarkSwitch.setChecked(sharedPreferences.getBoolean("value", false));
+        if (mDarkSwitch.isChecked()) {
+            mDarkSwitch.setChecked(true);
+        } else {
+            mDarkSwitch.setChecked(false);
+        }
+    }
+
+    //save preferences
+    private void savePreferences(String key, boolean value) {
+        SharedPreferences sharedPreferences = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(key, value);
+        editor.commit();
     }
 
 }

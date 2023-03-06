00fe7104cca646c39b845439cd04ac6d40594736
Simon7896 <55372007+Simon7896@users.noreply.github.com>
2023-03-06 00:08:43 -0800

    Identified bug where on rotation grid redraws cell word incorrectly

--- app/src/main/java/com/example/sudokuvocabulary/SudokuView.java
+++ app/src/main/java/com/example/sudokuvocabulary/SudokuView.java
@@ -4,14 +4,14 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.os.Bundle;
-import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
-import java.util.ArrayList;
+// TODO:
+// Replace current int matrix for storing cells to draw with string matrix
+// Re-write methods to work with new string matrix
 
 public class SudokuView extends View {
     private final int mGridColour;
@@ -21,8 +21,13 @@ public class SudokuView extends View {
     private int mCellSize;
     private final int mGridSideLength = 9;
     private final int mSubGridSize = 3;
+    // Stores index of word to write at a particular cell,
+    // Positive values indicate cell is filled with English,
+    // Negative values indicate cell is filled with Chinese
     private int[][] mCellsToDraw;
-
+    // Stores the words to draw and their translations as pairs in an array
+    // i.e. mCellsToDraw[0][0] is the english word of word one
+    // mCellsToDraw[0][1] is the translation for word one
     private String[][] mWordsToDraw;
 
     public SudokuView(Context context, @Nullable AttributeSet attrs) {
@@ -72,12 +77,12 @@ public class SudokuView extends View {
 
         drawGrid(canvas);
 
-        drawCellNumbers(canvas);
+        drawCellItems(canvas);
     }
 
-
+    // Sets the translated word to be drawn at the specified cell
     public void setCellToDraw(int row, int column, int value) {
-        mCellsToDraw[row][column] = value;
+        mCellsToDraw[row][column] = -value;
     }
 
     public int getCellSize() {
@@ -113,7 +118,7 @@ public class SudokuView extends View {
         }
     }
 
-    public void drawCellNumbers(Canvas canvas) {
+    public void drawCellItems(Canvas canvas) {
         for (int row = 0; row < mGridSideLength; row++) {
             for (int column = 0; column < mGridSideLength; column++) {
                 int cellValue = mCellsToDraw[row][column];

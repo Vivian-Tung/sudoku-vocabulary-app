a43a275b64b5d3ccd0cc15d74b66ae5ed523aafb
Simon Yu <simonyu180@gmail.com>
2023-03-08 12:09:51 -0800

    Added back button listener

--- app/src/main/java/com/example/sudokuvocabulary/WordCategoryActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/WordCategoryActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,7 +19,7 @@ public class WordCategoryActivity extends AppCompatActivity implements View.OnCl
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_word);
+        setContentView(R.layout.activity_word_category);
         tableName = getIntent().getStringExtra(getString(R.string.new_table_name_key));
 
         db = new DBAdapter(this);
@@ -30,13 +29,30 @@ public class WordCategoryActivity extends AppCompatActivity implements View.OnCl
         for (String category: categories) {
             addCategoryButton(category);
         }
+
+        Button backButton = findViewById(R.id.word_category_back_button);
+        backButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+        if (view.equals(findViewById(R.id.word_category_back_button))) {
+            onBackButtonClick();
+        } else {
+            onCategoryButtonClick(view);
+        }
+    }
+
+    private void onBackButtonClick() {
+        Intent intent = new Intent(WordCategoryActivity.this, WordListNameActivity.class);
+        startActivity(intent);
+    }
+
+    // Passes the category clicked to the word selection activity
+    private void onCategoryButtonClick(View view) {
         Button button = (Button) view;
         Intent intent = new Intent(WordCategoryActivity.this,
-        AnimalCategoryActivity.class);
+                AddWordsActivity.class);
         intent.putExtra(getString(R.string.new_table_name_key), tableName);
         intent.putExtra(getString(R.string.category_key), button.getText().toString());
         startActivity(intent);

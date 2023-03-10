53d7c4693895c79269ceb20991492668e8f0311b
Sam Zeng* <bsz1@sfu.ca>
2023-03-10 13:15:47 -0800

    Implemented the toolbar into set sudoku size activity

--- app/src/main/java/com/example/sudokuvocabulary/SetSudokuSizeActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/SetSudokuSizeActivity.java
@@ -12,7 +12,9 @@ import android.widget.ImageView;
 
 import androidx.appcompat.widget.Toolbar;
 
-public class SetSudokuSizeActivity extends AppCompatActivity {
+public class SetSudokuSizeActivity extends AppCompatActivity implements View.OnClickListener{
+
+    DBAdapter db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,22 @@ public class SetSudokuSizeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         setupTutorialButton();
+
+        db = new DBAdapter(this);
+        db.open();
+
+        Button btn_4x4 = findViewById(R.id.btn_4x4);
+        btn_4x4.setOnClickListener(this);
+
+        Button btn_6x6 = findViewById(R.id.btn_6x6);
+        btn_6x6.setOnClickListener(this);
+
+        Button btn_9x9 = findViewById(R.id.btn_9x9);
+        btn_6x6.setOnClickListener(this);
+
+        Button btn_12x12 = findViewById(R.id.btn_12x12);
+        btn_12x12.setOnClickListener(this);
+
     }
 
     private void setupTutorialButton() {
@@ -33,4 +51,23 @@ public class SetSudokuSizeActivity extends AppCompatActivity {
             startActivity(intent);
         });
     }
+
+    @Override
+    public void onClick(View v) {
+        Cursor cursor  = db.getAllRows("animals");
+                WordDictionary dictionary = new WordDictionary();
+                while(!cursor.isAfterLast()) {
+                    String word = cursor.getString(
+                            cursor.getColumnIndexOrThrow("word"));
+                    String translation = cursor.getString(
+                            cursor.getColumnIndexOrThrow("translation"));
+                    dictionary.add(word, translation);
+                    cursor.moveToNext();
+                }
+                cursor.close();
+                Intent intent = WordListsActivity.newIntent(
+                        SetSudokuSizeActivity.this, dictionary);
+                startActivity(intent);
+    }
+
 }

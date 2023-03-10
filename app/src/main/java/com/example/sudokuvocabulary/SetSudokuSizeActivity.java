49bb10cc064539775414d749637ecaea3d2b750c
Sam Zeng* <bsz1@sfu.ca>
2023-03-10 10:42:10 -0800

    Made a new activity for setting sudoku size

---
+++ app/src/main/java/com/example/sudokuvocabulary/SetSudokuSizeActivity.java
@@ -0,0 +1,14 @@
+package com.example.sudokuvocabulary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetSudokuSizeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_sudoku_size);
+    }
+}

52d3d7d1d416ca2b95b36c1afc4a80baa39a21a5
Sam Zeng* <bsz1@sfu.ca>
2023-03-10 11:57:34 -0800

    Implemented the toolbar into set sudoku size activity

--- app/src/main/java/com/example/sudokuvocabulary/SetSudokuSizeActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/SetSudokuSizeActivity.java
@@ -2,7 +2,15 @@ package com.example.sudokuvocabulary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class SetSudokuSizeActivity extends AppCompatActivity {
 
@@ -10,5 +18,19 @@ public class SetSudokuSizeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_sudoku_size);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        setupTutorialButton();
+    }
+
+    private void setupTutorialButton() {
+        ImageView tutorialBtn = findViewById(R.id.tutorialBtn);
+        tutorialBtn.setOnClickListener(view -> {
+
+            Intent intent = new Intent(SetSudokuSizeActivity.this, TutorialActivity.class);
+            startActivity(intent);
+        });
     }
 }

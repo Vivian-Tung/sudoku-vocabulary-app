bf7a826560d03a795f09f27a9a796b0a1dfcc2be
Simon Yu <simonyu180@gmail.com>
2023-04-07 16:53:57 -0700

    Re-purposed pref util to be more generic for use with game saving

--- app/src/main/java/com/example/sudokuvocabulary/utils/PrefUtils.java
+++ app/src/main/java/com/example/sudokuvocabulary/utils/PrefUtils.java
@@ -3,18 +3,10 @@ package com.example.sudokuvocabulary.utils;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import androidx.appcompat.app.AppCompatDelegate;
-
 public class PrefUtils {
 
-    private final Context context;
-
-    public PrefUtils(Context context) {
-        this.context = context;
-    }
-
     //save preferences
-    public void savePreferences(String key, boolean value) {
+    public static void saveBoolPreference(Context context, String key, boolean value) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(key, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putBoolean(key, value);
@@ -22,14 +14,8 @@ public class PrefUtils {
     }
 
     //load saved preferences
-    public boolean loadSavedPreferences(Context context, String key){
+    public static boolean loadBoolPreference(Context context, String key){
         SharedPreferences sharedPreferences = context.getSharedPreferences(key, Context.MODE_PRIVATE);
-        boolean darkMode = sharedPreferences.getBoolean(key, false);
-        if (darkMode) { //dark mode on
-            AppCompatDelegate.setDefaultNightMode((AppCompatDelegate.MODE_NIGHT_YES));
-        } else {
-            AppCompatDelegate.setDefaultNightMode((AppCompatDelegate.MODE_NIGHT_NO));
-        }
-        return darkMode;
+        return sharedPreferences.getBoolean(key, false);
     }
 }

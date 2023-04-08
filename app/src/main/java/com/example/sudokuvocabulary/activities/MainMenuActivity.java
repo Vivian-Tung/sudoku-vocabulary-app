84db1ccc9d12dcbf7b829731ba5c60efaaf63b01
Simon Yu <simonyu180@gmail.com>
2023-04-08 13:46:37 -0700

    Added comments to improve readability

--- app/src/main/java/com/example/sudokuvocabulary/activities/MainMenuActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/activities/MainMenuActivity.java
@@ -16,25 +16,42 @@ public class MainMenuActivity extends MenuForAllActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Set up play button
         Button playButton = findViewById(R.id.main_menu_play_button);
         playButton.setOnClickListener(v -> {
-            Intent intent = new Intent (MainMenuActivity.this, SelectModeActivity.class);
+            Intent intent = new Intent (
+                    MainMenuActivity.this,
+                    SelectModeActivity.class
+            );
             startActivity(intent);
         });
 
+        // Set up load button
         Button loadButton = findViewById(R.id.load_save_button);
         boolean saveExists = PrefUtils.loadBoolPreference(this, getString(R.string.save_game_key));
         loadButton.setEnabled(saveExists);
         loadButton.setOnClickListener(view -> {
             Intent intent;
-            if ((Boolean) SaveFileUtil.readFromSave(this, getString(R.string.save_game_file), SaveFileUtil.SaveObjects.GAME_MODE)) {
+
+            // Retrieve the game mode from the save file
+            boolean savedGameMode = (boolean)  SaveFileUtil.readFromSave(
+                    this,
+                    getString(R.string.save_game_file),
+                    SaveFileUtil.SaveObjects.GAME_MODE
+            );
+
+            // Check which mode the game was in according to the save file
+            if (savedGameMode) {
+                // Saved game was in listening mode
                 intent = new Intent(MainMenuActivity.this, ListenModeActivity.class);
             } else {
+                // Saved game was in normal mode
                 intent = new Intent(MainMenuActivity.this, SudokuActivity.class);
             }
             startActivity(intent);
         });
 
+        // Set up word bank button
         Button wordBankButton = findViewById(R.id.main_menu_word_bank_button);
         wordBankButton.setOnClickListener(view -> {
             Intent intent = new Intent(MainMenuActivity.this, WordListsActivity.class);

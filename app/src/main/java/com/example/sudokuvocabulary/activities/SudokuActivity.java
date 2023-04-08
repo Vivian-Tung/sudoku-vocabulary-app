3362eb9789bdae78ccb9eb4f698c5165c444164f
Simon Yu <simonyu180@gmail.com>
2023-04-08 13:46:09 -0700

    Added comments to improve readability

--- app/src/main/java/com/example/sudokuvocabulary/activities/SudokuActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/activities/SudokuActivity.java
@@ -1,8 +1,6 @@
 package com.example.sudokuvocabulary.activities;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -11,19 +9,15 @@ import com.example.sudokuvocabulary.R;
 
 public class SudokuActivity extends BaseSudokuActivity {
 
-    @SuppressLint("ClickableViewAccessibility")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
     @Override
     public View.OnClickListener onClick() {
         return view -> {
+            // Get the user selected word from the button
             mChoicePicked = (String) ((Button) view).getText();
             String toastMessage;
 
             if (isCorrect()) {
+                // Fill the cell with the selected word
                 mSudokuModel.checkAndFillCellAt(mCellRow, mCellColumn, mCellValue);
                 mSudokuView.setWordToDrawAt(mCellRow, mCellColumn, mTranslations[mCellValue-1]);
                 mSudokuView.invalidate();
@@ -31,8 +25,12 @@ public class SudokuActivity extends BaseSudokuActivity {
             } else {
                 toastMessage = getString(R.string.game_incorrect_toast_text);
             }
+
+            // Hide the input prompt, and tell user if they are correct or not
             mQuestionCard.setVisibility(View.GONE);
             Toast.makeText(view.getContext(), toastMessage, Toast.LENGTH_SHORT).show();
+
+            // Stop the game if the entire board has been filled
             if (mSudokuModel.isGridFilled()) {
                 timer.stop();
                 Intent intent = newIntent(view.getContext(), mWords, mTranslations);

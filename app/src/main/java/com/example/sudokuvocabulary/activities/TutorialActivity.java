4dddf7d2aa8b4bce358362b95c4c37c70c7bd7ee
Simon Yu <simonyu180@gmail.com>
2023-04-08 13:24:23 -0700

    Added comment

--- app/src/main/java/com/example/sudokuvocabulary/activities/TutorialActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/activities/TutorialActivity.java
@@ -11,6 +11,7 @@ public class TutorialActivity extends MenuForAllActivity {
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
         super.onPrepareOptionsMenu(menu);
+        // Hide the question mark button when inside the tutorial activity
         if (menu.findItem(R.id.action_tutorialBtn) != null) {
             menu.findItem(R.id.action_tutorialBtn).setVisible(false);
             return true;

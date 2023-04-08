41beea8dcd94749236d4f868ab78d1bb65341a2e
Simon Yu <simonyu180@gmail.com>
2023-04-08 13:23:24 -0700

    Randomized words for new game, added comments

--- app/src/main/java/com/example/sudokuvocabulary/activities/SetSudokuSizeActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/activities/SetSudokuSizeActivity.java
@@ -12,6 +12,8 @@ import com.example.sudokuvocabulary.adapters.DBAdapter;
 import com.example.sudokuvocabulary.models.WordDictionaryModel;
 import com.example.sudokuvocabulary.utils.PrefUtils;
 
+import java.util.Random;
+
 public class SetSudokuSizeActivity extends MenuForAllActivity implements View.OnClickListener{
 
     DBAdapter db;
@@ -23,6 +25,7 @@ public class SetSudokuSizeActivity extends MenuForAllActivity implements View.On
         db = new DBAdapter(this);
         db.open();
 
+        // Set up the buttons for each grid size
         Button btn_4x4 = findViewById(R.id.btn_4x4);
         btn_4x4.setOnClickListener(this);
 
@@ -34,7 +37,6 @@ public class SetSudokuSizeActivity extends MenuForAllActivity implements View.On
 
         Button btn_12x12 = findViewById(R.id.btn_12x12);
         btn_12x12.setOnClickListener(this);
-
     }
 
     @Override
@@ -45,34 +47,40 @@ public class SetSudokuSizeActivity extends MenuForAllActivity implements View.On
     }
 
     @Override
-    public void onClick(View v) {
-        int size = 0;
-        switch (v.getId()) {
-            case R.id.btn_4x4:
-                size = 4;
-                break;
-            case R.id.btn_6x6:
-                size = 6;
-                break;
-            case R.id.btn_9x9:
-                size = 9;
-                break;
-            case R.id.btn_12x12:
-                size = 12;
-                break;
-        }
+    public void onClick(View view) {
+        // Figure out which size has been selected from the button text
+        String text = ((Button) view).getText().toString();
+        int size = Integer.parseInt(text.substring(0, text.indexOf('x')));
+
+        // Calculate the sub grid widths and heights
         int subWidth = (int) Math.ceil(Math.sqrt(size));
         int subHeight = (int) Math.floor(Math.sqrt(size));
+
+        // Get words from the main word table
         Cursor cursor  = db.getAllRows("words");
         WordDictionaryModel dictionary = new WordDictionaryModel();
-        while(cursor.moveToNext() && size-- > 0) {
+        Random random = new Random();
+        while(size > 0) {
+            // Get a random word from the database
+            cursor.moveToPosition(random.nextInt(cursor.getCount()));
             String word = cursor.getString(
                     cursor.getColumnIndexOrThrow("word"));
             String translation = cursor.getString(
                     cursor.getColumnIndexOrThrow("translation"));
-            dictionary.add(word, translation);
+
+            // Add it to the game if it hasn't already been added
+            if (!dictionary.contains(word)) {
+                dictionary.add(word, translation);
+                size--;
+            }
         }
         cursor.close();
+
+        // Tell the game to not load from the save file
+        PrefUtils.saveBoolPreference(this, getString(R.string.save_game_key), false);
+
+        // Launch the normal mode or listening mode
+        // depending on what the user previously selected
         Intent intent;
         if (getIntent().getBooleanExtra(getString(R.string.mode_key), false)) {
             intent = new Intent(this, ListenModeActivity.class);
@@ -84,7 +92,6 @@ public class SetSudokuSizeActivity extends MenuForAllActivity implements View.On
         }
         intent.putExtra(getString(R.string.sub_width_key), subWidth);
         intent.putExtra(getString(R.string.sub_height_key), subHeight);
-        PrefUtils.saveBoolPreference(this, getString(R.string.save_game_key), false);
         startActivity(intent);
     }
 

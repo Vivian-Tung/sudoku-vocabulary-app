130f913f1f5b88e156cbb52832f6fb3ac9315856
Simon Yu <simonyu180@gmail.com>
2023-04-08 13:32:46 -0700

    Added comments

--- app/src/main/java/com/example/sudokuvocabulary/activities/WordListNameActivity.java
+++ app/src/main/java/com/example/sudokuvocabulary/activities/WordListNameActivity.java
@@ -21,29 +21,43 @@ public class WordListNameActivity extends MenuForAllActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        EditText text = findViewById(R.id.table_name_input);
-        Button confirmButton = findViewById(R.id.confirm_name_button);
+        EditText text = findViewById(R.id.table_name_input); // Box for user input
 
+        Button confirmButton = findViewById(R.id.confirm_name_button);
         confirmButton.setOnClickListener(view -> {
+            // Get the new list name from the input box
             String listName = text.getText().toString();
 
+            // Initialize the database
             db = new DBAdapter(this);
             db.open();
+
+            // Check if the list name already exists, notify user if so
             if (db.getTableNames().contains(listName.toLowerCase(Locale.ROOT))) {
-                Toast.makeText(this,
+                Toast.makeText(
+                        this,
                         "List name already used",
-                        Toast.LENGTH_SHORT).show();
+                        Toast.LENGTH_SHORT).show(
+                );
+
+            // Check if list name is valid, move to the word category selection menu
             } else if (listName.matches("[A-Za-z0-9]+")) {
                 Intent intent = new Intent(
                         WordListNameActivity.this, WordCategoryActivity.class);
                 intent.putExtra(getString(R.string.new_table_name_key), listName);
                 startActivity(intent);
+
+            // If the input box is empty, notify user
             } else if (listName.length() == 0) {
-                Toast.makeText(this,
+                Toast.makeText(
+                        this,
                         "List name cannot be empty",
-                        Toast.LENGTH_SHORT).show();
+                        Toast.LENGTH_SHORT).show(
+                );
+            //The list name is invalid, notify the user
             } else {
-                Toast.makeText(this,
+                Toast.makeText(
+                        this,
                         "List name cannot have spaces or special characters",
                         Toast.LENGTH_SHORT).show();
             }
